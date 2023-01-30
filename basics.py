3e38bc08aade99bbf4d07c8848271018015e587c
GenyaElamkov <GenyaElamkov@gmail.com>
2023-01-30 15:01:37 +0300

    [upd] basic - > first commit.

--- basics.py
+++ basics.py
@@ -17,10 +17,29 @@ def hide_card(card_number):
     return 12*'*' + card_number.replace(" ", "")[12:]
 
 
+def same_parity(numbers):
+    """
+    Реализуйте функцию same_parity(), которая принимает один аргумент:
+
+    numbers — список целых чисел Функция должна возвращать новый список,
+    элементами которого являются числа из списка numbers, имеющие ту же
+    четность, что и первый элемент этого списка.
+    """
+    if not numbers:
+        return []
+
+    if numbers[0] % 2 == 0:
+        return list(filter(lambda x: x % 2 == 0, numbers))
+    else:
+        return list(filter(lambda x: x % 2 == 1, numbers))
+
+    # return [i for i in nums if i % 2 == nums[0] % 2]
+
 def main():
-    card = '3456 9012 5678 1234'
+    print(same_parity([]))
+    # card = '3456 9012 5678 1234'
     # card = '1234567890123456'
-    print(hide_card(card))
+    # print(hide_card(card))
 
 
 if __name__ == '__main__':
