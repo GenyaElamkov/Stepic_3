deefc0b4ed284c7a8fdc785031569059bc0b283a
GenyaElamkov <GenyaElamkov@gmail.com>
2023-05-15 21:08:48 +0300

    [upd] flow part 4.4 -> 9 close

---
+++ Module_4/lesson_4.4_step9_json_insert_key.py
@@ -0,0 +1,30 @@
+"""
+Добавляет в каждый JSON-объект из данного списка все недостающие ключи,
+присваивая этим ключам значение null. Ключ считается недостающим,
+если он присутствует в каком-либо другом объекте, но отсутствует в данном.
+Программа должна создать список с обновленными JSON-объектами и записать
+его в файл updated_people.json.
+
+https://stepik.org/lesson/623073/step/9?unit=618703
+"""
+import json
+
+with (
+    open("people.json", "r", encoding="utf-8") as f,
+    open("updated_people.json", "w", encoding="utf-8") as out_f,
+):
+    data = json.load(f)
+
+    arr_key = sorted(set([k for dic in data for k in dic.keys()]))
+
+    all_dic = []
+    for dic in data:
+        tmp = {}
+        for k in arr_key:
+            tmp[k] = None
+            if k in dic:
+                tmp[k] = dic[k]
+
+        all_dic.append(tmp)
+
+    json.dump(all_dic, out_f, indent=3)
