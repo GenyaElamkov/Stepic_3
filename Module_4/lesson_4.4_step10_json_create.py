f3d7d66df3f0294e3f25c19b34b598ada027821c
GenyaElamkov <GenyaElamkov@gmail.com>
2023-05-16 12:58:43 +0300

    [upd] flow part 4.4 -> 10 close

---
+++ Module_4/lesson_4.4_step10_json_create.py
@@ -0,0 +1,22 @@
+"""
+Cоздает единственный JSON-объект, имеющий в качестве ключа название религии,
+а в качестве значения — список стран, в которых исповедуется данная религия.
+Полученный JSON-объект программа должна записать в файл religion.json
+
+https://stepik.org/lesson/623073/step/10?unit=618703
+"""
+
+import json
+
+with (
+    open("countries.json", "r", encoding="utf-8") as f,
+    open("religion.json", "w", encoding="utf-8") as out_file,
+):
+    data = json.load(f)
+    all_region = {}
+    for dic in data:
+        all_region[dic["religion"]] = all_region.setdefault(dic["religion"], []) + [
+            dic["country"]
+        ]
+
+    json.dump(all_region, out_file, indent=3)
