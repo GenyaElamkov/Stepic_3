edb2ca13ff6899b51255c839088d7e1df434f970
GenyaElamkov <GenyaElamkov@gmail.com>
2023-05-29 16:09:29 +0300

    [upd] flow part 4.5 -> 17 close

---
+++ Module_4/lesson_4.5_step17_zip_name_files.py
@@ -0,0 +1,17 @@
+"""
+Выводит названия файлов из этого архива, которые были созданы или изменены
+позднее 2021-11-30 14:22:00. Названия файлов должны быть расположены в
+лексикографическом порядке, каждое на отдельной строке.
+"""
+
+from datetime import datetime
+from zipfile import ZipFile
+
+flag_date = datetime.strptime("2021-11-30 14:22:00", "%Y-%m-%d %H:%M:%S")
+with ZipFile("workbook.zip") as zip_file:
+    result = []
+    for item in zip_file.infolist():
+        if not item.is_dir() and datetime(*item.date_time) > flag_date:
+            result.append(item.filename.split('/')[-1])
+
+print(*sorted(result), sep='\n')
