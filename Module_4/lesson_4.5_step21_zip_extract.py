3eeb0efdceb78e1aa61798e8d6ff4adb09ebc9dd
GenyaElamkov <GenyaElamkov@gmail.com>
2023-05-30 12:32:09 +0300

    [upd] flow part 4.5 -> 21 close

---
+++ Module_4/lesson_4.5_step21_zip_extract.py
@@ -0,0 +1,28 @@
+"""
+Извлекать файлы *args из архива zip_name в папку с программой.
+Если в функцию не передано ни одного названия файла для извлечения,
+то функция должна извлечь все файлы из архива.
+"""
+
+from zipfile import ZipFile
+
+def extract_this(zip_name, *args):
+    with ZipFile(zip_name, mode="a") as zip_file:
+        if not args:
+            zip_file.extractall()
+        else:
+            for file in args:
+                zip_file.extract(file)
+
+#
+# def extract_this(zip_name: str, *args):
+#     if not args:
+#         args = None
+#     with ZipFile(zip_name) as zf:
+#         zf.extractall(members=args)
+
+
+
+if __name__ == "__main__":
+    extract_this('workbook.zip', 'earth.jpg', 'exam.txt')
+    # extract_this('workbook.zip')
