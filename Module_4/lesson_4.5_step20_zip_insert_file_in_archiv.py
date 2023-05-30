f39aedc3ea690c65a7fc9b5de7a188722682fc11
GenyaElamkov <GenyaElamkov@gmail.com>
2023-05-30 11:58:51 +0300

    [upd] flow part 4.5 -> 20 close

--- Module_4/lesson_4.5_step20_zip_insert_file_in_archiv.py
+++ Module_4/lesson_4.5_step20_zip_insert_file_in_archiv.py
@@ -1,6 +1,20 @@
-def main():
-    pass
+"""
+Добавил в архив files.zip только те файлы из списка file_names, объем
+которых не превышает 100 байт.
+"""
 
+import os.path
+from zipfile import ZipFile
 
-if __name__ == '__main__':
-    main()
+file_names = ['how to prove.pdf', 'fipi_demo_2022.pdf',
+              'Hollow Knight Silksong.exe',
+              'code.jpeg', 'stepik.png', 'readme.txt', 'shopping_list.txt',
+              'Alexandra Savior – Crying All the Time.mp3', 'homework.py',
+              'test.py']
+
+with ZipFile("files.zip", mode="a") as zip_file:
+    for file_name in file_names:
+        if os.path.getsize(file_name) < 100:
+            zip_file.write(file_name)
+
+# print(type(os.path.getsize("lesson_4.5_step19_zip_setup_archivpy.py")))
