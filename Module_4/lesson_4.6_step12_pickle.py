e4da00b303b6168abaed659c4adfbf6ec42104cf
GenyaElamkov <GenyaElamkov@gmail.com>
2023-06-02 13:14:54 +0300

    [upd] flow part 4.6 -> 12 close

---
+++ Module_4/lesson_4.6_step12_pickle.py
@@ -0,0 +1,13 @@
+"""
+Исправить ошибки.
+
+https://stepik.org/lesson/584474/step/12?unit=579234
+"""
+
+import pickle
+
+dogs = {'Ozzy': 2, 'Filou': 7, 'Luna': 4, 'Skippy': 11, 'Barco': 13,
+        'Balou': 10, 'Laika': 15}
+
+with open('dogs.pkl', mode='wb') as file:
+    pickle.dump(dogs, file)
