9c9dfc907ed263391aaa3044d13fff63bacb6eff
GenyaElamkov <genyaelamkov@gmail.com>
2024-01-20 17:38:32 +0300

    lesson 11.6 step9-11

--- Module_10_Iterators_generators/test.py
+++ Module_10_Iterators_generators/test.py
@@ -1,7 +1,14 @@
-def high_and_low(numbers):
-    nms = list(map(int, numbers.split()))
-    return f"{max(nms)} {min(nms)}"
+def last_digit(n1, n2):
+    if n2 == 0:
+        return 0
+    elif n2 == 1:
+        return n1 % 10
+    elif n2 % 2 != 0:
+        return n1 * last_digit(n1, n2 - 1) % 10
+    elif n2 % 2 == 0:
+        return last_digit(n1 * n1, n2 / 2) % 10
+    
 
 
-res = high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4")
-print(res)
+# print(last_digit(2**200, 2**300))
+print(last_digit(4, 2))
